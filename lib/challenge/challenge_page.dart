50c9ae6d32d07ac36c3d9a63c6a25b91124213a6
Anderson David <andersondavidti@hotmail.com>
2021-05-31 20:54:19 -0300

    Class 04 - 39 min

--- lib/challenge/challenge_page.dart
+++ lib/challenge/challenge_page.dart
@@ -1,5 +1,6 @@
 import 'dart:ffi';
 
+import 'package:dev_quiz/challenge/widgets/next_button/next_button-widget.dart';
 import 'package:dev_quiz/challenge/widgets/question_indicator/question_indicator_widget.dart';
 import 'package:dev_quiz/home/quiz_widget.dart';
 import 'package:flutter/material.dart';
@@ -18,6 +19,32 @@ class _ChallengePageState extends State<ChallengePage> {
         preferredSize: Size.fromHeight(60),
       ),
       body: QuizWidget(title: "O que o Flutter faz em sua totalidade?"),
+      bottomNavigationBar: SafeArea(
+        bottom: true,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 5.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              Expanded(
+                child: NextButtonWidget.white(
+                  label: "Fácil",
+                  onTap: () {},
+                ),
+              ),
+              SizedBox(
+                width: 7,
+              ),
+              Expanded(
+                child: NextButtonWidget.green(
+                  label: "Confirmar",
+                  onTap: () {},
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
--- lib/home/home_page.dart
+++ lib/home/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:dev_quiz/challenge/challenge_page.dart';
 import 'package:dev_quiz/core/app_colors.dart';
 import 'package:dev_quiz/home/home_controller.dart';
 import 'package:dev_quiz/home/home_state.dart';
@@ -60,12 +61,20 @@ class _HomePageState extends State<HomePage> {
                   mainAxisSpacing: 10,
                   crossAxisCount: 2,
                   children: controller.quizzes!
-                      .map((e) => QuizCardWidget(
-                            title: e.title,
-                            percent: e.questionAnswered / e.questions.length,
-                            completed:
-                                "${e.questionAnswered} de ${e.questions.length}",
-                          ))
+                      .map(
+                        (e) => QuizCardWidget(
+                          title: e.title,
+                          percent: e.questionAnswered / e.questions.length,
+                          completed:
+                              "${e.questionAnswered} de ${e.questions.length}",
+                          onTap: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => ChallengePage()));
+                          },
+                        ),
+                      )
                       .toList(),
                 ),
               ),
